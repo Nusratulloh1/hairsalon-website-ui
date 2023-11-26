8f8f458b9a523c798da85d2ee3074cac2504ad16
nusratullo_a <axunovnusrat@gmail.com>
2023-11-26 22:59:26 +0500

    some test

--- layouts/components/Header.vue
+++ layouts/components/Header.vue
@@ -1,13 +1,5 @@
 <template>
     <div>
-        <!-- Spinner Start -->
-        <!-- <div id="spinner"
-            class="show bg-dark position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
-            <div class="spinner-grow text-primary" style="width: 3rem; height: 3rem;" role="status">
-                <span class="sr-only">Loading...</span>
-            </div>
-        </div> -->
-        <!-- Spinner End -->
         <!-- Navbar Start -->
         <nav class="navbar navbar-expand-lg bg-secondary navbar-dark sticky-top py-lg-0 px-lg-5 wow fadeIn"
             data-wow-delay="0.1s">
