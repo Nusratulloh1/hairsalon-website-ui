07874a7337a4f1eda07896564f638667d7a72688
nusratullo_a <axunovnusrat@gmail.com>
2023-11-26 15:41:37 +0500

    moved public file

--- app.vue
+++ app.vue
@@ -33,19 +33,19 @@ useHead({
       rel: "stylesheet"
     },
     {
-      href: "../assets/lib/animate/animate.min.css",
+      href: "/lib/animate/animate.min.css",
       rel: "stylesheet"
     },
     {
-      href: "../assets/lib/owlcarousel/assets/owl.carousel.min.css",
+      href: "/lib/owlcarousel/assets/owl.carousel.min.css",
       rel: "stylesheet"
     },
     {
-      href: "../assets/styles/css/bootstrap.min.css",
+      href: "/styles/css/bootstrap.min.css",
       rel: "stylesheet"
     },
     {
-      href: "../assets/styles/css/style.css",
+      href: "/styles/css/style.css",
       rel: "stylesheet"
     }
   ],
@@ -57,19 +57,19 @@ useHead({
       src: "https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"
     },
     {
-      src: "../assets/lib/wow/wow.min.js"
+      src: "/lib/wow/wow.min.js"
     },
     {
-      src: "../assets/lib/easing/easing.min.js"
+      src: "/lib/easing/easing.min.js"
     },
     {
-      src: "../assets/lib/waypoints/waypoints.min.js"
+      src: "/lib/waypoints/waypoints.min.js"
     },
     {
-      src: "../assets/lib/owlcarousel/owl.carousel.min.js"
+      src: "/lib/owlcarousel/owl.carousel.min.js"
     },
     {
-      src: "../assets/lib/main.js"
+      src: "/lib/main.js"
     }
   ]
 })
